377fa6b13204d1767f03c19d09eef59f242d2484
Alandersonjs <limaalanderson@gmail.com>
2023-09-20 21:20:11 -0300

    data problema

--- Register.php
+++ Register.php
@@ -6,17 +6,40 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nome = $_POST["nome"];
     $email = $_POST["email"];
     $senha = $_POST["senha"];
+    $estado = $_POST["estado"];
+    $cidade = $_POST["cidade"];
+    $telefone = $_POST["telefone"];
+    
+    // Coletar a data de nascimento no formato dd/mm/yyyy
+    $data_nascimento = $_POST["data_nascimento"];
 
-    // Inserir dados do usuário no banco de dados
-    $sql = "INSERT INTO usuarios (nome, email, senha) 
-    VALUES ('$nome', '$email', '$senha')";
-
-    if(mysqli_query($conexao, $sql)){
+    // Inserir dados na tabela usuarios
+    $query_usuarios = "INSERT INTO usuarios (nome, email, senha, estado, cidade, telefone, data_nascimento) VALUES (?, ?, ?, ?, ?, ?, ?)";
+    
+    $stmt = $conexao->prepare($query_usuarios);
+    $stmt->bind_param("sssssss", $nome, $email, $senha, $estado, $cidade, $telefone, $data_nascimento);
+    
+    if ($stmt->execute()) {
         echo "Usuário cadastrado com sucesso";
+        exit; // linha para interromper a execução do código aqui
+    } else {
+        echo "Erro: " . $stmt->error;
     }
-        else{
-            echo "Erro".mysqli_connect_error($conexao);
+
+    //A DATA NÃO ESTÁ SENDO SALVA NO FORMATO PADRÃO BRASILEIRO DIA/MÊS/ANO
+
+    $query_usuario = "SELECT id, email, DATE_FORMAT(data_nascimento, '%d/%m/%Y') AS data_formatada, data_nascimento FROM usuarios";
+    
+    $result_usuario = $conexao->query($query_usuario);
+
+    if ($result_usuario) {
+        while ($row_notification = $result_usuario->fetch_assoc()) {
+            var_dump($row_notification);
         }
-        mysqli_close($conexao);
+    } else {
+        echo "Erro na consulta de notificações: " . $conexao->error;
+    }
+
+    $conexao->close();
 }
 ?>
