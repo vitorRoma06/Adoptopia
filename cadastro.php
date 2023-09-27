64cc519a95423ae9e07eb9ae0f389a35237a44cd
vitorRoma06 <vitorroma14@gmail.com>
2023-09-27 14:47:57 -0300

    login-registro

---
+++ cadastro.php
@@ -0,0 +1,92 @@
+<?php
+include('conexao.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro-Adoptopia</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body class="body-register">
+    <div class="form-box-register">
+        <div class="button-close">
+            <a href="index.html"><img src="imgs/close.png" alt="close"></a>
+        </div>
+        <h1 class="title-form bold">Cadastrar</h1>
+
+        <form class="form-login-principal flex-column" action="valida-registro.php" method="POST">
+
+            <div class="div-inputs flex-column">
+                <div class="step step-1 active">
+                    <p class="sub-title-etapa bold">Etapa 1 de 2</p>
+                    <div class="div-buttons-social flex-column j-content align-itens">
+                        <button type="button" class="button-social flex-row align-itens j-content bold"><img
+                                src="imgs/google-icon.png" alt="">
+                            <p>Cadastrar pelo Google</p>
+                        </button>
+                        <button type="button" class="button-social flex-row align-itens j-content bold"><img
+                                src="imgs/fc-icon.png" alt="">
+                            <p>Cadastrar pelo Facebook</p>
+                        </button>
+                    </div>
+
+                    <div class="mid-form flex-row align-itens j-content">
+                        <hr>ou
+                        <hr>
+                    </div>
+                    <div class="form-group flex-column">
+                        <label for="nome">Nome:</label>
+                        <input type="text" name="nome" required> <br>
+
+                        <label for="email">E-mail:</label>
+                        <input type="email" name="email" required> <br>
+
+                        <label for="password">Senha:</label>
+                        <input type="password" name="senha" required>
+                        <div>
+                            <button type="button" class="prox-button-register bold">PRÓXIMO</button>
+                        </div>
+                        <p id="opcao">Você tem uma conta? <a id="amarelo" href="login.php">Faça Login</a></p>
+                    </div>
+
+                </div>
+                <div class="step step-2">
+                    <div class="form-group flex-column  ">
+                    <p class="sub-title-etapa bold">Etapa 2 de 2</p>
+                        <label for="estado">Estado:</label>
+                        <input type="text" name="estado" required> <br>
+
+                        <label for="cidade">Cidade:</label>
+                        <input type="text" name="cidade" required> <br>
+
+                        <label for="telefone">Telefone:</label>
+                        <input type="tel" name="telefone" required> <br>
+
+                        <label for="data_nascimento">Data de nascimento:</label>
+                        <input type="date" name="data_nascimento" required>
+                        <div class="group-buttons flex-row">
+                            <button type="button" class="voltar-button-register bold">VOLTAR</button>
+                            <button type="submit" class="cadastrar-button bold">CADASTRAR</button>
+
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
+        </form>
+
+
+    </div>
+    <script src="js/multistep.js"></script>
+</body>
+
+</html>
--- login.php
+++ login.php
@@ -1,35 +1,98 @@
 <?php
-//buscar as informações armazenadas em memória referente a saída de informações
-ob_start();
+include('conexao.php');
+
+function teste()
+{
+
+    include('conexao.php');
+    if (isset($_POST['email']) || isset($_POST['senha'])) {
+        if (strlen($_POST['email'] == 0) || strlen($_POST['senha'] == 0)) {
+            echo "Preencha seu email e senha.";
+        } else if (strlen($_POST['email']) == 0) {
+            echo "Preencha seu email.";
+        } else if (strlen($_POST['senha']) == 0) {
+            echo "Preencha sua senha.";
+        } else {
+
+            $email = $mysqli->real_escape_string($_POST['email']);
+            $senha = $mysqli->real_escape_string($_POST['senha']);
+
+            $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
+            $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
+
+            $qt = $sql_query->num_rows;
+
+            if ($qt == 1) {
+
+                $usuario = $sql_query->fetch_assoc();
+
+                if (!isset($_SESSION)) {
+                    session_start();
+                }
+
+                $_SESSION['id'] = $usuario['id'];
+                $_SESSION['nome'] = $usuario['nome'];
+
+                header("Location: index.html");
+
+
+            } else {
+                echo "Falha ao logar! E-mail ou senha incorretos";
+            }
+        }
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Login-Adoptopia</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
-<body>
-    <?php 
-        include("conexao.php");
-        //verificar formulario para autenticação
-        $senha = $_POST["senha"];
-        $email = $_POST["email"];
 
-        $sql = "SELECT nome FROM usuarios WHERE email='$email' AND senha='$senha'";
-        //inicializar a verificação e a sessão
-        $resultado =mysqli_query($conexao, $sql);
-        $linhas=mysqli_affected_rows($conexao);
+<body class="body-login">
+    <div class="form-box">
+        <div class="button-close">
+            <a href="index.html"><img src="imgs/close.png" alt="close"></a>
+        </div>
+        <h1 class="title-form bold">Login</h1>
+        <form class="form-login-principal flex-column" action="login.php" method="POST">
+            <div class="div-buttons-social flex-column j-content align-itens">
+                <button type="button" class="button-social flex-row align-itens j-content bold"><img
+                        src="imgs/google-icon.png" alt="">
+                    <p>Login pelo Google</p>
+                </button>
+                <button type="button" class="button-social flex-row align-itens j-content bold"><img
+                        src="imgs/fc-icon.png" alt="">
+                    <p>Login pelo Facebook</p>
+                </button>
+            </div>
 
-            if($linhas>0){
-                session_start();
-                $_SESSION["usuario"]=$email;
-                //redirecionar as informações para outra página
-                header("location: index.html");
-            } else {
-                echo "Dados incorretos! <br>" ;
-            }
-    ?>
+            <div class="mid-form flex-row align-itens j-content">
+                <hr>ou
+                <hr>
+            </div>
+            <div class="div-inputs flex-column">
+
+                <input type="text" name="email" required="" placeholder="Digite seu e-mail">
+                <input type="password" name="senha" required="" autocomplete="off" placeholder="Digite sua senha">
+                <p class="fail-login">
+                    <?php teste() ?>
+                </p>
+
+                <a href="#" class="forgot-button">Esqueceu sua senha?</a>
+
+                <button type="submit" class="login-button bold">ENTRAR</button>
+            </div>
+        </form>
+        <p id="opcao">Você não tem uma conta? <a id="amarelo" href="cadastro.php">Cadastre-se</a></p>
+    </div>
 </body>
+
 </html>
